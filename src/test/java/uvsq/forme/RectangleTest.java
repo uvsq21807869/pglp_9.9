a08d4fcaa75041c04ee5b3ee66ab6b4edb26db8f
Amina ayachi <21807869@ens.uvsq.fr>
2020-05-20 10:17:24 +0200

    hiérarchie des formes avec le pattern composite et les tests

---
+++ src/test/java/uvsq/forme/RectangleTest.java
@@ -0,0 +1,30 @@
+package uvsq.forme;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import uvsq.bean.Cercle;
+import uvsq.bean.Point;
+import uvsq.bean.Rectangle;
+
+public class RectangleTest {
+
+
+
+	@Test
+	public void testMove() {
+
+		Rectangle r1 = new Rectangle("r1", new Point(7, 20), 20, 10);
+		r1.move(7, 5);
+		assertEquals(r1.getPointDOrigine().getPosition(), "(7, 5)");
+	}
+	
+	@Test
+	public void testDeplacerDirection() {
+		Rectangle car1 = new Rectangle("rectangle", new Point(11, 20), 5,6);
+		car1.deplacerDirection(1, 1);
+		assertEquals(12, car1.getPointDOrigine().getX());
+		assertEquals(21, car1.getPointDOrigine().getY());
+	}
+}
